94350fc8b8813ea038a6591f3223949007d0d8ca
KPOFS <gimbhs@gmail.com>
2025-09-18 14:34:32 +0900

    도로 노선 수정

--- public/drawWay.js
+++ public/drawWay.js
@@ -9,120 +9,129 @@ let nmap;
 const motorwayClass = 'mw';
 const highwayClass = 'hw';
 
+
 export function initWays() {
-    nmap = getMap();
-    wayinfowindow = new naver.maps.InfoWindow({
-        content: ''
-    });
+	nmap = getMap();
+	wayinfowindow = new naver.maps.InfoWindow({
+		content: ''
+	});
 
-    const params = [
-        ['road/Motorways', '#FF0000', true, motorwayClass, document.getElementById("motorways"), 'solid', 3],
-        ['road/highway', '#0000FF', true, highwayClass, document.getElementById("highway"), 'solid', 3]
-    ];
+	const params = [
+		['road/Motorways', '#FF0000', true, motorwayClass, document.getElementById("motorways"), 'solid', 3],
+		['road/highway', '#0000FF', true, highwayClass, document.getElementById("highway"), 'solid', 3]
+	];
 
-    for (const p of params) {
-        const [wayType, clor, init_status, clss, part, strokestyle, strokeweight] = p;
+	for (const p of params) {
+		const wayType = p[0]
+		const clor = p[1]
+		const init_status = p[2]
+		const clss = p[3]
+		const part = p[4]
+		const strokestyle = p[5]
+		const strokeweight = p[6]
 
-        fetch(wayType + '.json')
-            .then(res => res.json())
-            .then(jso => {
-                for (let wayName in jso) {
-                    const wayNameMangle = wayName + clss;
+		fetch(wayType + '.json')
+			.then(res => res.text())
+			.then(str => JSON.parse(str))
+			.then(jso => {
+				for (let wayName in jso) {
+					const wayNameMangle = wayName + clss
 
-                    const linestr_coord = jso[wayName];
-                    const linestr_LatLng = linestr_coord.map(
-                        x => new naver.maps.LatLng(x[1], x[0])
-                    );
+					let linestr_coord = jso[wayName]
 
-                    const polline = new naver.maps.Polyline({
-                        path: linestr_LatLng,
-                        strokeColor: clor,
-                        strokeStyle: strokestyle,
-                        strokeLineCap: 'round',
-                        strokeLineJoin: 'round',
-                        strokeWeight: strokeweight,
-                        clickable: true
-                    });
+					let linestr_LatLng = []
+					for (let x of linestr_coord)
+						linestr_LatLng.push(new naver.maps.LatLng(x[1], x[0]))
 
-                    // ways에 저장
-                    ways[wayNameMangle] = {
-                        "type": wayType,
-                        "overlays": [polline]
-                    };
+					let polline = new naver.maps.Polyline({
+						path: linestr_LatLng,
+						strokeColor: clor,
+						strokeStyle: strokestyle,
+						strokeLineCap: 'round',
+						strokeLineJoin: 'round',
+						strokeWeight: strokeweight,
+						clickable: true
+					});
 
-                    // 마우스 이벤트
-                    naver.maps.Event.addListener(polline, 'mouseover', e => {
-                        if (!polline.getMap()) return; // 지도에 없으면 무시
-                        polline.setOptions({
-                            strokeWeight: strokeweight,
-                            strokeColor: 'yellow'
-                        });
-                        wayinfowindow.setOptions({
-                            position: e.coord,
-                            content: wayName
-                        });
-                        wayinfowindow.open(nmap, e.coord);
-                    });
+					ways[wayNameMangle] = {
+						"type": wayType,
+						"overlays": [polline]
+					}
 
-                    naver.maps.Event.addListener(polline, 'mouseout', _ => {
-                        if (!polline.getMap()) return;
-                        polline.setOptions({
-                            strokeWeight: strokeweight,
-                            strokeColor: clor
-                        });
-                        wayinfowindow.close();
-                    });
+					naver.maps.Event.addListener(polline, 'mouseover', e => {
+						polline.setOptions({
+							strokeWeight: strokeweight,
+							strokeColor: 'yellow',
+						});
+						wayinfowindow.setOptions({
+							position: e.coord,
+							content: wayName
+						})
+						wayinfowindow.open(nmap, e.coord);
+					});
 
-                    // 체크박스 생성
-                    const ckbx = document.createElement('input');
-                    ckbx.type = "checkbox";
-                    ckbx.id = wayNameMangle;
-                    ckbx.className = clss;
-                    if (init_status) ckbx.checked = true;
+					naver.maps.Event.addListener(polline, 'mouseout', _ => {
+						polline.setOptions({
+							strokeWeight: strokeweight,
+							strokeColor: clor
+						});
+						wayinfowindow.close();
+					});
 
-                    const y = document.createElement('label');
-                    y.style.color = clor;
-                    y.setAttribute('for', wayNameMangle);
-                    y.textContent = wayName;
+					let ckbx = document.createElement('input')
+					ckbx.setAttribute("type", "checkbox")
+					if (init_status)
+						ckbx.setAttribute("checked", true)
+					ckbx.setAttribute('id', wayNameMangle)
+					ckbx.setAttribute('class', clss)
 
-                    ckbx.onchange = _ => {
-                        if (ckbx.checked) addWay(wayNameMangle);
-                        else delWay(wayNameMangle);
-                    };
+					let y = document.createElement('label')
+					y.setAttribute('style', 'color: ' + clor)
+					y.setAttribute('for', wayNameMangle)
+					y.appendChild(document.createTextNode(wayName))
 
-                    part.appendChild(ckbx);
-                    part.appendChild(y);
-                    part.appendChild(document.createElement('br'));
+					ckbx.onchange = _ => {
+						if (ckbx.checked)
+							addWay(wayNameMangle)
+						else
+							delWay(wayNameMangle)
+					}
 
-                    if (init_status) addWay(wayNameMangle);
-                }
-            });
+					part.appendChild(ckbx)
+					part.appendChild(y)
+					part.appendChild(document.createElement('br'))
+
+					if (init_status)
+						addWay(wayNameMangle)
+				}
+			})
+	}
+}
+
+const overlays_setMap = (wayName, m) => {
+	for (let overlay of ways[wayName]['overlays']) {
+		overlay.setMap(m)
+	}
+}
+
+const addWay = wayName => { overlays_setMap(wayName, nmap) }
+const delWay = wayName => { overlays_setMap(wayName, null) }
+
+const selct = params => {
+    const checkboxes = document.querySelectorAll('input[class="' + params[0] + '"]');
+    for (let ckbx of checkboxes) {
+        if (ckbx.checked !== params[1]) {
+            ckbx.checked = params[1];
+            if (params[1]) {
+                addWay(ckbx.id);
+            } else {
+                delWay(ckbx.id);
+            }
+        }
     }
 }
 
-// 지도에 표시/삭제
-const overlays_setMap = (wayName, m) => {
-    if (!ways[wayName]) return;
-    for (let overlay of ways[wayName]['overlays']) {
-        overlay.setMap(m);
-    }
-};
-
-const addWay = wayName => overlays_setMap(wayName, nmap);
-const delWay = wayName => overlays_setMap(wayName, null);
-
-// 클래스 단위 전체 on/off
-const selct = (clss, checkedState, action) => {
-    const checkboxes = document.querySelectorAll(`input.${clss}`);
-    for (let ckbx of checkboxes) {
-        if (ckbx.checked === checkedState) {
-            ckbx.checked = !checkedState;
-            action(ckbx.id);
-        }
-    }
-};
-
-export const selectMW = _ => selct(motorwayClass, false, addWay);
-export const deselectMW = _ => selct(motorwayClass, true, delWay);
-export const selectHW = _ => selct(highwayClass, false, addWay);
-export const deselectHW = _ => selct(highwayClass, true, delWay);
+export const selectMW = _ => selct([motorwayClass, true]);
+export const deselectMW = _ => selct([motorwayClass, false]);
+export const selectHW = _ => selct([highwayClass, true]);
+export const deselectHW = _ => selct([highwayClass, false]);
